96b327f754e815c05b0b4c958d9af3051adef697
Geert Pasteels <geert.pasteels@gmail.com>
2010-08-31 17:05:15 -0700

    added authentication decorator

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
 
 class Controller( webapp.RequestHandler ):
+    def __init__(self, _users=None):
+        """constructor injection or something that looks
+        like it"""
+        self.users = _users or users
+
     def get(self, *args):
         self.exe_get(self.http_get, *args)
 
-    def exe_get(self, t, *args):
-        get_response = t(*args)
+    def exe_get(self, http_get, *args):
+        """Based on the return type of http_get a
+        response is created"""
+        get_response = http_get(*args)
 
         #String
         if type(get_response) == str:
@@ -32,7 +39,65 @@ class Template(object):
         self.file = file
         self.data = data
 
-#APPLICATION
+#Decorators
+def authenticated(f):
+
+    def wrapper(self, *args, **kwargs):
+        user = self.users.get_current_user()
+
+        if not user:
+            return Redirect(self.users.create_login_url(self.request.uri))
+
+        return f(self, *args, **kwargs)
+
+    return wrapper
+
+#def authenticated(_users=None, _request=None):
+#    def wrap(f):
+#        def wrapped_f(*args):
+#            users = _users or self.users
+#            request = _request or self.request
+#
+#            user = users.users.get_current_user()
+#            if not user:
+#                return Redirect(self.users.create_login_url(self.request.uri))
+#
+#            f(*args)
+#        return wrapped_f
+#    return wrap
+#
+class Authenticated(object):
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, _users=None, _request=None, *args):
+        self.users = _users or self.f.users
+        self.request = _request or self.f.request
+
+        user = self.users.get_current_user()
+        if not user:
+            return Redirect(self.users.create_login_url(self.request.uri))
+
+        self.f()
+
+import functools
+def auth(method, _users=None, _request=None):
+    @functools.wraps(method) 
+    def wrapper(self, *args, **kwargs):
+        if _users:
+            self.users = _users
+
+        if _request:
+            self.request = _request
+
+        user = self.users.get_current_user()
+        if not user:
+            return Redirect(self.users.create_login_url(self.request.uri))
+
+        return method(self, *args, **kwargs)
+    return wrapper
+
+#RequestHandlers
 class Home(Controller):
     def http_get(self):
         return 'Hello world!'
@@ -46,11 +111,6 @@ class AboutUs(Controller):
         return Template('/about_us.html', 'We are the robots')
 
 class Login(Controller):
-    def __init__(self, _users=None):
-        if _users:
-            self.users = _users
-        else:
-            self.users = users
 
     def http_get(self):
         login_url = self.users.create_login_url('/')
@@ -60,13 +120,19 @@ class News(Controller):
     def http_get(self, word):
         return word
 
+class UserZone(Controller):
+    @authenticated
+    def http_get(self):
+        return self.users.get_current_user().nickname()
 
+#URLS
 def application():
     return webapp.WSGIApplication([ ( '/', Home ),
                                     ( '/contact', Contact ),
                                     ( '/about_us', AboutUs ),
                                     ( '/login', Login ),
                                     ( '/news/([-\w]+)', News ),
+                                    ( '/userzone', UserZone ),
                                   ], debug=True)
 
 def main():
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Home, Contact, AboutUs, Login, News
+from main import Home, Contact, AboutUs, Login, News, Authenticated, Redirect, authenticated, auth
 from mock import Mock
 
 class TestApplication(unittest.TestCase):
@@ -31,3 +31,34 @@ class TestApplication(unittest.TestCase):
     def test_news(self):
         news = News()
         assert news.http_get('the bird is the word') == 'the bird is the word'
+
+class TestDecorators(unittest.TestCase):
+    def test_Authenticated_no_user(self):
+        #Arrange
+        f = Mock()
+        this = Mock()
+        this.users.get_current_user.return_value = None
+        this.users.create_login_url.return_value = '/login'
+
+        #Act
+        decorator = authenticated(f)
+        result = decorator(this)
+
+        #Assert
+        assert not f.called
+        assert this.users.get_current_user.called
+        assert type(result) == Redirect
+
+    def test_Authenticated_logged_in(self):
+        #Arrange
+        f = Mock()
+        this = Mock()
+        this.users.get_current_user.return_value = True
+
+        #Act
+        decorator = authenticated(f)
+        decorator(this)
+
+        #Assert
+        assert this.users.get_current_user.called
+        assert f.called
